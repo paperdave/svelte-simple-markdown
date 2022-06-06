01310bec9a1dd0b8773600316881bfb8f12cb730
Dave Caruso <dave@davecode.net>
2022-06-05 23:20:17 -0500

    fix list rendering

--- src/lib/svelte/renderers/ListRenderer.svelte
+++ src/lib/svelte/renderers/ListRenderer.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import type { ASTNode } from '$lib/core';
+	import MarkdownNode from '../MarkdownNode.svelte';
 
-	export let node: ASTNode;
+	export let node: { ordered: boolean; content: ASTNode[][]; start: number };
 </script>
 
-<svelte:element this={node.ordered ? 'ol' : 'ul'} start={node.start}><slot /></svelte:element>
+<svelte:element this={node.ordered ? 'ol' : 'ul'} start={node.start}>
+	{#each node.content ?? [] as child}
+		<li><MarkdownNode node={child} /></li>
+	{/each}
+</svelte:element>
